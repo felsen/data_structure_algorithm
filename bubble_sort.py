31a5d03f16bfb00b8f44194ba07c313b4bcf56e6
Felix Stephen <felsen88@gmail.com>
2017-08-07 16:09:11 +0530

    Modified: changes done for bubble chart using while loop

--- bubble_sort.py
+++ bubble_sort.py
@@ -28,3 +28,22 @@ def bubble_sort_n(alist):
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20, ]
 print bubble_sort_n(alist)
 
+
+def bubble_sort_m(alist):
+    exchange = True
+    passnum = len(alist) - 1
+    while passnum > 0 and exchange:
+        exchange = False
+        for i in range(passnum):
+            if alist[i] > alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+        passnum -= 1
+
+
+alist = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110, ]
+bubble_sort_m(alist)
+print alist
+
+
