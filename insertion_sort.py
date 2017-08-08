e4b7c117e304f6d2d7ff721b2b01b4a1f8d0563b
Felix Stephen <felsen88@gmail.com>
2017-08-08 17:07:24 +0530

    Modified: added insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,6 +4,16 @@ Insertion sort.
 
 
 def insertion_sort(alist):
-    print alist
-    return "Inserted."
+    for index in xrange(1, len(alist)):
+        current_value = alist[index]
+        position = index
+        while position > 0 and alist[position - 1] > current_value:
+            alist[position] = alist[position - 1]
+            position -= 1
+        alist[position] = current_value
+    return alist
+
+
+alist = [54, 26, 93, 17, 77, 31, 44, 55, 20, ]
+print insertion_sort(alist)
 
